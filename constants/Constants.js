9804a86990b3be1071e4df43bdde866726295218
pavithra-0131 <160534599+pavithra-0131@users.noreply.github.com>
2024-02-19 10:54:09 -0800

    Initial commit

---
+++ constants/Constants.js
@@ -0,0 +1,17 @@
+const nameToPic = {
+	riatao: ["Ria Tao", require("../assets/MemberPictures/riatao.jpg")],
+	miladbrown: ["Milad Brown", require("../assets/MemberPictures/miladbrown.jpg")],
+	rianamistry: ["Riana Mistry", require("../assets/MemberPictures/rianamistry.jpg")],
+	landymanzanero: ["Landy Manzanero", require("../assets/MemberPictures/landymanzanero.png")],
+	subhammitra: ["Subham Mitra", require("../assets/MemberPictures/subhammitra.jpeg")],
+	angiezhu: ["Angie Zhu", require("../assets/MemberPictures/angiezhu.png")],
+	edangortzak: ["Edan Gortzak", require("../assets/MemberPictures/edangortzak.jpg")],
+	nadahameed: ["Nada Hameed", require("../assets/MemberPictures/nadahameed.JPG")],
+	morrellnioble: ["Morrell Nioble", require("../assets/MemberPictures/morrellnioble.jpeg")],
+	pavithraarunanand: ["Pavitra Arun Anand", require("../assets/MemberPictures/pavithraarunanand.png")],
+	estherluan: ["Esther Luan", require("../assets/MemberPictures/estherluan.JPG")],
+	danielgil: ["Daniel Gil", require("../assets/MemberPictures/danielgil.jpg")],
+	denniskoh: ["Dennis Koh", require("../assets/MemberPictures/denniskoh.jpeg")],
+	mikeguerrero: ["Mike Guerrero", require("../assets/MemberPictures/mikeguerrero.jpeg")]
+}
+export { nameToPic }
